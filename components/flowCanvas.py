3957f075c11c6813b7dc586e85555c7b09dc434e
Theo Fernandes dos Santos Dias <l0n3l.dev@gmail.com>
2024-02-12 16:50:02 -0300

    Canvas Object
    Created a canvas object and moved the flow code to it.
    
    Also added a option to reset the canvas position when needed

--- components/flowCanvas.py
+++ components/flowCanvas.py
@@ -1,6 +1,77 @@
-from PyQt6.QtWidgets import QWidget
-from PyQt6.QtCore import QRect
+from PyQt6.QtWidgets import QWidget, QApplication, QHBoxLayout
+from PyQt6.QtGui import QMouseEvent, QPalette, QColor
+from PyQt6.QtCore import QRect, QPoint, Qt
+from components.flowStep import FlowStep
+from PIL import Image
+import os
 
 class FlowCanvas(QWidget):
     def __init__(self, parent:QWidget):
         super().__init__(parent)
+        self.setGeometry(QRect(0, 0, 10000, 10000))
+
+        palette = QPalette()
+        palette.setColor(QPalette.ColorRole.Window, QColor("#ffffff"))
+        self.setPalette(palette)
+        self.setAutoFillBackground(True)
+
+        self.layout = QHBoxLayout(self)
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout.setSpacing(30)
+
+        self.drag = QPoint()
+
+        self.resetCanvasPosition()
+    
+    def resetCanvasPosition(self):
+        screenGeometry = QApplication.primaryScreen().geometry()
+        flowX = (screenGeometry.width() - self.width()) // 2
+        flowY = (screenGeometry.height() - self.height()) // 2
+        self.move(flowX, flowY)
+    
+    def updateFlow(self, loadedFlow):
+        while self.layout.count():
+            item = self.layout.takeAt(0)
+            widget = item.widget()
+            if widget:
+                widget.deleteLater()
+
+        for stepId, step in enumerate(loadedFlow.flow, start=0):
+            if stepId == loadedFlow.flow.__len__() - 1:
+                self.layout.addWidget(FlowStep(step.name, step.icon))
+            else:
+                self.layout.addWidget(FlowStep(step.name, step.icon, True))
+
+    def exportImages(self, path):
+        flowFolder = os.path.join(path, 'flow_images')
+        os.makedirs(flowFolder, exist_ok=True)
+
+        for i in range(self.layout.count()):
+            step = self.layout.itemAt(i).widget()
+
+            if isinstance(step, FlowStep):
+                img = Image.fromqpixmap(step.generateImage())
+                img.save(os.path.join(flowFolder, f'flowStep{i}.png'))
+
+                print(img.size)
+    
+    #moving the flow canvas
+    def mousePressEvent(self, event: QMouseEvent):
+        self.drag = event.globalPosition().toPoint() - self.pos()
+
+    def mouseMoveEvent(self, event):
+        if not self.drag.isNull():
+            position = event.globalPosition().toPoint() - self.drag
+
+            limitX = [-2000, -6000]
+            limitY = [-3950, -5000]
+
+            move_x = max(limitX[1], min(position.x(), limitX[0]))
+            move_y = max(limitY[1], min(position.y(), limitY[0]))
+
+            new_position = QPoint(move_x, move_y)
+
+            self.move(new_position)
+    
+    def mouseReleaseEvent(self, event):
+        self.drag = QPoint()
--- src/screens/flowCreator.py
+++ src/screens/flowCreator.py
@@ -1,10 +1,5 @@
-import os
-from PyQt6.QtGui import QMouseEvent, QResizeEvent
-from PyQt6.QtWidgets import QPushButton, QWidget, QHBoxLayout, QApplication, QFileDialog
-from PyQt6.QtCore import Qt, QRect, QPoint
-from components.flowStep import FlowStep
+from PyQt6.QtWidgets import QPushButton, QWidget, QFileDialog
 from components.flowCanvas import FlowCanvas
-from PIL import Image
 from src.main.flow import Flow
 
 class FlowCreator(QWidget):
@@ -15,20 +10,7 @@ class FlowCreator(QWidget):
         self.setup()
 
     def setup(self):
-        self.flow = QWidget(self)
-        self.flow.setGeometry(QRect(0, 0, 10000, 10000))
-        self.flow.setStyleSheet('background-color: #ffffff;')
-
-        screenGeometry = QApplication.primaryScreen().geometry()
-        flowX = (screenGeometry.width() - self.flow.width()) // 2
-        flowY = (screenGeometry.height() - self.flow.height()) // 2
-        self.flow.move(flowX, flowY)
-
-        self.flowLayout = QHBoxLayout(self.flow)
-        self.flowLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.flowLayout.setSpacing(30)
-
-        self.drag = QPoint()
+        self.flow = FlowCanvas(self)
 
         addFlowBtn = QPushButton('Adicionar Etapa', self)
         addFlowBtn.setGeometry(100, 20, 200, 50)
@@ -41,68 +23,25 @@ class FlowCreator(QWidget):
         exportBtn = QPushButton('Exportar Imagens', self)
         exportBtn.setGeometry(500, 20, 200, 50)
         exportBtn.clicked.connect(self.openExportWindow)
-    
-    def updateFlow(self):
-        while self.flowLayout.count():
-            item = self.flowLayout.takeAt(0)
-            widget = item.widget()
-            if widget:
-                widget.deleteLater()
 
-        for stepId, step in enumerate(self.loadedFlow.flow, start=0):
-            if stepId == self.loadedFlow.flow.__len__() - 1:
-                self.flowLayout.addWidget(FlowStep(step.name, step.icon))
-            else:
-                self.flowLayout.addWidget(FlowStep(step.name, step.icon, True))
+        resetCanvasBtn = QPushButton('Resetar Posição do Fluxo', self)
+        resetCanvasBtn.setGeometry(700, 20, 200, 50)
+        resetCanvasBtn.clicked.connect(self.flow.resetCanvasPosition)
     
     def addFlowStep(self):
         self.loadedFlow.addStep()
-        self.updateFlow()
+        self.flow.updateFlow(self.loadedFlow)
     
     def removeFlowStep(self):
         self.loadedFlow.deleteStep()
-        self.updateFlow()
+        self.flow.updateFlow(self.loadedFlow)
     
     def openExportWindow(self):
         docFile = None
 
-        if self.flowLayout.count() > 0:
+        if self.loadedFlow.flow.__len__() > 0:
             docFile = QFileDialog.getExistingDirectory(self, 'Selecione a pasta para exportar as imagens')
         
         if docFile:
-            self.exportImages(docFile)
-    
-    def exportImages(self, path):
-        flowFolder = os.path.join(path, 'flow_images')
-        os.makedirs(flowFolder, exist_ok=True)
-
-        for i in range(self.flowLayout.count()):
-            step = self.flowLayout.itemAt(i).widget()
-
-            if isinstance(step, FlowStep):
-                img = Image.fromqpixmap(step.generateImage())
-                img.save(os.path.join(flowFolder, f'flowStep{i}.png'))
-
-                print(img.size)
-    
-    #moving the flow canvas
-    def mousePressEvent(self, event: QMouseEvent):
-        child_widget = self.childAt(event.pos())
-        if child_widget and child_widget == self.flow and event.button() == Qt.MouseButton.LeftButton:
-            self.drag = event.pos() - self.flow.pos()
-
-    def mouseMoveEvent(self, event):
-        if self.drag and not self.drag.isNull():
-            new_position = event.pos() - self.drag
-
-            limitX = [-2000, -6000]
-            limitY = [-3950, -5000]
-
-            move_x = max(limitX[1], min(new_position.x(), limitX[0]))
-            move_y = max(limitY[1], min(new_position.y(), limitY[0]))
-
-            self.flow.move(move_x, move_y)
-    
-    def mouseReleaseEvent(self, event):
-        self.drag = QPoint()
+            self.flow.exportImages(docFile)
 
